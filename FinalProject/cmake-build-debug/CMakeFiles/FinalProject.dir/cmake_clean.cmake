e1cdf8946195b89b0051336589e365b295c211c0
rileyk64 <rkunkel@mymail.mines.edu>
2021-05-03 15:36:54 -0600

    Add files via upload

---
+++ FinalProject/cmake-build-debug/CMakeFiles/FinalProject.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FinalProject.dir/DataVector.cpp.obj"
+  "CMakeFiles/FinalProject.dir/PokemonData.cpp.obj"
+  "CMakeFiles/FinalProject.dir/main.cpp.obj"
+  "FinalProject.exe"
+  "FinalProject.exe.manifest"
+  "FinalProject.pdb"
+  "libFinalProject.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FinalProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
